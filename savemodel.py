a56077b23dfa883b7944b81dd60530754632c2be
zhyvr <46420014+zhyvr@users.noreply.github.com>
2023-06-23 13:25:43 +0300

    updates :]

---
+++ savemodel.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+df =pd.read_csv("data/dataset.csv")
+df.head()
+
+df['sentiment'] = df['sentiment'].replace({'positive': 1, 'negative': -1})
+df.head()
+
+x = df['sentence']
+y = df['sentiment']
+
+vectorizer = CountVectorizer()
+x_train_vectorized = vectorizer.fit_transform(x)
+
+model = LogisticRegression()
+model.fit(x_train_vectorized, y)
+
+
+# Save the model and vectorizer to disk
+filename = 'model.pkl'
+pickle.dump((model, vectorizer), open(filename, 'wb'))
